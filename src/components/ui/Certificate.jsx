da39ca7a0d68f0f16e5e791363605910c8010c65
Abhi-sinpie <abhishekmudata@gmail.com>
2025-08-09 22:50:51 +0530

    Fixed certification responsiveness css

--- src/components/ui/Certificate.jsx
+++ src/components/ui/Certificate.jsx
@@ -3,13 +3,16 @@ import React from "react";
 const Certificate = ({ name, link }) => {
   return (
     <figure className="flex justify-between items-center px-4 text-center py-2 border border-borderColor rounded-xl">
-      <h2 className="text-textColor font-semibold cursor-default">
+      <h2
+        className="w-1/2 truncate text-textColor font-semibold cursor-default"
+        title={name} // shows full name on hover
+      >
         {name}
       </h2>
       <a
         target="_blank"
         href={link}
-        className="cursor-pointer text-textColor border border-borderColor px-4 py-2 rounded-xl hover:bg-secondaryColor hover:text-textSecond transition-all"
+        className="w-1/2 text-center cursor-pointer text-textColor border border-borderColor px-4 py-2 rounded-xl hover:bg-secondaryColor hover:text-textSecond transition-all"
       >
         View Certification
       </a>
